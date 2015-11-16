1d2713db0adbcf87a6be3a3de9e37ad4bfbad8e1
looooo <sppedflyer@gmail.com>
2015-11-16 21:51:45 +0100

    renamed package

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from freecad_dist import fc_install         # custom freecad install
 from distutils.core import setup
 
-import glider_gui
+import freecad_glider
 
 setup(cmdclass={'install': fc_install},
       install_requires=["OpenGlider"],     
       name='freecad_glider',
-      version=glider_gui.__version__,
+      version=freecad_glider.__version__,
       description='gui for openglider in freecad',
       url='http://github.com/hiaselhans/OpenGlider',
       author='booya',
       license='LGPL2',
-      packages=["glider_gui", "glider_gui/tools", "glider_gui/icons"],
+      packages=["freecad_glider", "freecad_glider/tools", "freecad_glider/icons"],
       package_data = {"": ["*.svg", "*.json"]})
 
