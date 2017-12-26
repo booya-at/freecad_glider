6a57a02b527ecd315203687a7c34d9b1acb7d593
looooo <sppedflyer@gmail.com>
2017-12-26 20:53:13 +0100

    fix MANIFEST.in

--- freecad/freecad_glider/init_gui.py
+++ freecad/freecad_glider/init_gui.py
@@ -3,7 +3,7 @@ try:
     import FreeCADGui as Gui
     import FreeCAD
 except ImportError:
-    print("module not loaded with freecad")
+    print('module not loaded with freecad')
 
 
 from pivy import coin
@@ -25,49 +25,48 @@ except TypeError:
 
     coin.SoSeparator = newSeparator
 
-Dir = os.path.dirname(__file__)
-Gui.addIconPath(Dir + "/icons")
+Dir = os.path.abspath(os.path.dirname(__file__))
+Gui.addIconPath(os.path.join(Dir, 'icons'))
 
 
 class gliderWorkbench(Gui.Workbench):
-    """probe workbench object"""
-    MenuText = "glider"
-    ToolTip = "glider workbench"
-    Icon = "glider_workbench.svg"
+    MenuText = 'glider'
+    ToolTip = 'glider workbench'
+    Icon = 'glider_workbench.svg'
     toolBox = [
-        "CreateGlider",
-        "ImportGlider",
-        "ShapeCommand",
-        "ArcCommand",
-        "AoaCommand",
-        "ZrotCommand",
-        "AirfoilCommand",
-        "AirfoilMergeCommand",
-        "BallooningCommand",
-        "BallooningMergeCommand",
-        "CellCommand",
-        "LineCommand",
-        "CutCommand",
-        "ColorCommand",
-        "Gl2dExport"]
+        'CreateGlider',
+        'ImportGlider',
+        'ShapeCommand',
+        'ArcCommand',
+        'AoaCommand',
+        'ZrotCommand',
+        'AirfoilCommand',
+        'AirfoilMergeCommand',
+        'BallooningCommand',
+        'BallooningMergeCommand',
+        'CellCommand',
+        'LineCommand',
+        'CutCommand',
+        'ColorCommand',
+        'Gl2dExport']
 
     featureBox = [
-        "GliderRibFeatureCommand",
-        "GliderBallooningFeatureCommand",
-        "GliderSharkFeatureCommand",
-        "GliderSingleSkinRibFeatureCommand",
-        "GliderFlapFeatureCommand"]
+        'GliderRibFeatureCommand',
+        'GliderBallooningFeatureCommand',
+        'GliderSharkFeatureCommand',
+        'GliderSingleSkinRibFeatureCommand',
+        'GliderFlapFeatureCommand']
 
     productionBox = [
-        "PatternCommand",
-        "PanelCommand",
-        "PolarsCommand"]
+        'PatternCommand',
+        'PanelCommand',
+        'PolarsCommand']
 
     devBox = [
-        "RefreshCommand"]
+        'RefreshCommand']
 
     def GetClassName(self):
-        return "Gui::PythonWorkbench"
+        return 'Gui::PythonWorkbench'
 
     def Initialize(self):
         from . import tools
@@ -77,41 +76,41 @@ class gliderWorkbench(Gui.Workbench):
         Gui.addCommand('ShapeCommand', tools.ShapeCommand())
         Gui.addCommand('AirfoilCommand', tools.AirfoilCommand())
         Gui.addCommand('ArcCommand', tools.ArcCommand())
-        Gui.addCommand("AoaCommand", tools.AoaCommand())
-        Gui.addCommand("BallooningCommand", tools.BallooningCommand())
-        Gui.addCommand("LineCommand", tools.LineCommand())
+        Gui.addCommand('AoaCommand', tools.AoaCommand())
+        Gui.addCommand('BallooningCommand', tools.BallooningCommand())
+        Gui.addCommand('LineCommand', tools.LineCommand())
 
-        Gui.addCommand("ImportGlider", tools.ImportGlider())
-        Gui.addCommand("Gl2dExport", tools.Gl2dExport())
-        Gui.addCommand("AirfoilMergeCommand", tools.AirfoilMergeCommand())
-        Gui.addCommand("BallooningMergeCommand", tools.BallooningMergCommand())
-        Gui.addCommand("CellCommand", tools.CellCommand())
-        Gui.addCommand("ZrotCommand", tools.ZrotCommand())
-        Gui.addCommand("CutCommand", tools.CutCommand())
-        Gui.addCommand("ColorCommand", tools.ColorCommand())
+        Gui.addCommand('ImportGlider', tools.ImportGlider())
+        Gui.addCommand('Gl2dExport', tools.Gl2dExport())
+        Gui.addCommand('AirfoilMergeCommand', tools.AirfoilMergeCommand())
+        Gui.addCommand('BallooningMergeCommand', tools.BallooningMergCommand())
+        Gui.addCommand('CellCommand', tools.CellCommand())
+        Gui.addCommand('ZrotCommand', tools.ZrotCommand())
+        Gui.addCommand('CutCommand', tools.CutCommand())
+        Gui.addCommand('ColorCommand', tools.ColorCommand())
 
-        Gui.addCommand("PatternCommand", tools.PatternCommand())
-        Gui.addCommand("PanelCommand", tools.PanelCommand())
-        Gui.addCommand("PolarsCommand", tools.PolarsCommand())
+        Gui.addCommand('PatternCommand', tools.PatternCommand())
+        Gui.addCommand('PanelCommand', tools.PanelCommand())
+        Gui.addCommand('PolarsCommand', tools.PolarsCommand())
 
-        Gui.addCommand("GliderRibFeatureCommand", tools.GliderRibFeatureCommand())
-        Gui.addCommand("GliderBallooningFeatureCommand", tools.GliderBallooningFeatureCommand())
-        Gui.addCommand("GliderSharkFeatureCommand", tools.GliderSharkFeatureCommand())
-        Gui.addCommand("GliderSingleSkinRibFeatureCommand", tools.GliderSingleSkinRibFeatureCommand())
-        Gui.addCommand("GliderFlapFeatureCommand", tools.GliderFlapFeatureCommand())
+        Gui.addCommand('GliderRibFeatureCommand', tools.GliderRibFeatureCommand())
+        Gui.addCommand('GliderBallooningFeatureCommand', tools.GliderBallooningFeatureCommand())
+        Gui.addCommand('GliderSharkFeatureCommand', tools.GliderSharkFeatureCommand())
+        Gui.addCommand('GliderSingleSkinRibFeatureCommand', tools.GliderSingleSkinRibFeatureCommand())
+        Gui.addCommand('GliderFlapFeatureCommand', tools.GliderFlapFeatureCommand())
 
-        Gui.addCommand("RefreshCommand", tools.RefreshCommand())
+        Gui.addCommand('RefreshCommand', tools.RefreshCommand())
 
-        self.appendToolbar("GliderTools", self.toolBox)
-        self.appendToolbar("Production", self.productionBox)
-        self.appendToolbar("Feature", self.featureBox)
-        self.appendToolbar("Develop", self.devBox)
+        self.appendToolbar('GliderTools', self.toolBox)
+        self.appendToolbar('Production', self.productionBox)
+        self.appendToolbar('Feature', self.featureBox)
+        self.appendToolbar('Develop', self.devBox)
 
-        self.appendMenu("GliderTools", self.toolBox)
-        self.appendMenu("Production", self.productionBox)
-        self.appendMenu("Feature", self.featureBox)
+        self.appendMenu('GliderTools', self.toolBox)
+        self.appendMenu('Production', self.productionBox)
+        self.appendMenu('Feature', self.featureBox)
 
-        Gui.addPreferencePage(Dir + "/ui/preferences.ui", "Display")
+        Gui.addPreferencePage(Dir + '/ui/preferences.ui', 'Display')
 
     def Activated(self):
         pass
